57760dc2b6f04dd99c067628beea41a15648e73c
theguy20phi <46367025+theguy20phi@users.noreply.github.com>
2023-06-22 21:47:28 -0500

    Make basic setup.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Order Form Discord Bot
- Provides a Discord bot for managing the club's order form.
+ Provides a Discord bot for managing the club's order form, time sheet, and other tasks
+ that are common in Discord for our club.
--- bot.py
+++ bot.py
@@ -1,2 +1,19 @@
-print("Hello world!")
-print("This is a test.")
+import discord
+import json
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+with open("config.json", 'r') as config:
+    data = json.load(config)
+    client.run(data["token"])
