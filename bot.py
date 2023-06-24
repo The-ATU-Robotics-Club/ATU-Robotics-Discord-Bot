521ddc0735252d4269c65b29783316b24a3a508b
theguy20phi <bradenwepierce@gmail.com>
2023-06-23 20:38:54 -0500

    Add time sheet functionality.

--- bot.py
+++ bot.py
@@ -48,6 +48,13 @@ def query_db(command, params = None):
     db_connect.close()
     return response
 
+def modify_hours_db(id, hours):
+    response = query_db("SELECT HOURS FROM TIME_SHEET WHERE ID = %s", (id, ))
+    if not response:
+        modify_db("INSERT INTO TIME_SHEET (ID, Hours) VALUES (%s, %s)", (id, hours, ))
+    hours = max(0, response[0][0] + hours)
+    modify_db("UPDATE TIME_SHEET SET Hours = %s WHERE ID = %s", (hours, id, ))
+    
 
 @bot.event
 async def on_ready():
@@ -95,6 +102,51 @@ async def getorders(interaction: discord.Interaction):
 async def clearorders(interaction: discord.Interaction):
     modify_db("TRUNCATE ORDER_FORM")
     await interaction.response.send_message("Orders cleared!")
+
+
+@bot.tree.command(description = "Gets the work hours of the caller or user given.")
+async def gethours(interaction: discord.Interaction,
+                  user: discord.User = None):
+    if not user:
+        user = interaction.user
+    response = query_db("SELECT HOURS FROM TIME_SHEET WHERE ID = %s", (user.id, ))
+    hours = 0 if not response else response[0][0]
+    await interaction.response.send_message(f"User {user.name} has contributed {hours} hours.")
+
+
+@bot.tree.command(description = "Changes work hours to the caller or user given.")
+async def modifyhours(interaction: discord.Interaction,
+                      hours: int,
+                      user: discord.User = None):
+    if not user:
+        user = interaction.user
+    response = query_db("SELECT HOURS FROM TIME_SHEET WHERE ID = %s", (user.id, ))
+    if not response:
+        modify_db("INSERT INTO TIME_SHEET (ID, Hours) VALUES (%s, %s, %s)", (user.id, user.name, 0, ))
+    hours = max(0, response[0][0] + hours)
+    modify_db("UPDATE TIME_SHEET SET Hours = %s WHERE ID = %s", (hours, user.id, ))
+    await interaction.response.send_message(f"Set {user.name}'s hours to {hours} hours.")
+
+
+@bot.tree.command(description = "Gives a CSV file of the time sheet.")
+async def gettimesheet(interaction: discord.Interaction):
+    response = query_db("SELECT * FROM TIME_SHEET")
+    filename = "time_sheet.csv"
+    with open(filename, 'w') as file:
+        csvFile = csv.writer(file)
+        csvFile.writerow(["ID", "Name", "Hours"])
+        csvFile.writerows(response)
+    await interaction.response.send_message(file=discord.File(filename))
+    os.remove(filename)
+
+
+@bot.tree.command(description = "Clears all data from the time sheet if the user is an Admin.")
+@app_commands.checks.has_role("Admin")
+async def cleartimesheet(interaction: discord.Interaction):
+    if not user:
+        user = interaction.user
+    modify_db("TRUNCATE TIME_SHEET")
+    await interaction.response.send_message("Time sheet cleared!")
 
 
 bot.run(config["token"])
