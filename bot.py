f2f383af372c796625e0996425d3b769d7562171
theguy20phi <46367025+theguy20phi@users.noreply.github.com>
2024-04-05 19:47:02 -0500

    Fix bug.

--- bot.py
+++ bot.py
@@ -63,17 +63,17 @@ async def addorder(interaction: discord.Interaction,
                    quantity: int,
                    unitPrice: float):
     # Adds the new order into the form, NULL for ID to automatically increment.
-    roundedUnitPrice = math.ceil(unitPrice)
+    unitPrice = math.ceil(unitPrice)
     totalPrice = quantity * unitPrice
     modify_db("INSERT INTO ORDER_FORM (ID, Name, Website, Quantity, UnitPrice, TotalPrice) VALUES (NULL, %s, %s, %s, %s, %s)",
-              (name, website, quantity, roundedUnitPrice, totalPrice))
-    await interaction.response.send_message(f"Inserted ({name}, {website}, {quantity}, {roundedUnitPrice}, {totalPrice}) into order form!")
+              (name, website, quantity, unitPrice, totalPrice, ))
+    await interaction.response.send_message(f"Inserted ({name}, {website}, {quantity}, {unitPrice}, {totalPrice}) into order form!")
 
 
 @bot.tree.command(description="Removes an order from the order form based on its ID.")
 async def removeorder(interaction: discord.Interaction,
                       id: int):
-    modify_db("DELETE FROM ORDER_FORM WHERE ID = %s", id)
+    modify_db("DELETE FROM ORDER_FORM WHERE ID = %s", (id, ))
     await interaction.response.send_message(f"Order #{id} removed!")
 
 
